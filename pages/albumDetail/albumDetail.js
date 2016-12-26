35d8a295fa0b39663a8779bbf290ce4a2d19fdc6
Toxni <i@toxni.com>
2016-12-27 04:20:13 +0800

    feat(): finish album view

--- pages/albumDetail/albumDetail.js
+++ pages/albumDetail/albumDetail.js
@@ -5,9 +5,6 @@ Page({
 
   onLoad: function (options) {
     that = this;
-    that.setData({
-      items: DefaultData
-    })
 
     albumId = options.albumId;
     that.requestAlbumDetail();
@@ -15,7 +12,7 @@ Page({
 
   requestAlbumDetail: function () {
     wx.request({
-      url: 'https://URL',
+      url: 'https://ebichu.cn/albumView/',
       method: 'POST',
       header: {
         'content-type': 'application/x-www-from-urlencoded'
@@ -26,7 +23,7 @@ Page({
       },
       success: function (s) {
         console.log(s);
-        that.bindData(res.image);
+        that.bindData(s.image);
       },
       fail: function (f) {
         console.log(f);
@@ -48,19 +45,3 @@ Page({
 
 var albumId;
 var that;
-var DefaultData = [
-  {
-    "time": "2016.12.26",
-    "latitude": "东经143.56°",
-    "longitude": "北纬43.45°",
-    "image": "http://p4.image.hiapk.com/uploads/allimg/160914/7730-160914162257.jpg",
-    "content": "哈哈哈，玩得很开心哦，在这里第一次见到雪"
-  },
-  {
-    "time": "2016.12.29",
-    "latitude": "东经146.56°",
-    "longitude": "北纬42.45°",
-    "image": "http://jiangsu.china.com.cn/uploadfile/2015/0726/1437876596537968.jpg",
-    "content": "走在路上被一条叫做徐锐的狗咬了一口，我的天哪，赶紧回去打一针狂犬疫苗，好心情都没了"
-  }
-];
